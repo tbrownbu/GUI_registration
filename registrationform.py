2a0dbea007b946cdbc2344cee1e3fc87e8985781
Yun <yun_chuah1@baylor.edu>
2023-03-03 11:08:22 -0600

    first commit

---
+++ registrationform.py
@@ -0,0 +1,111 @@
+#include frames, labels, input box, 
+# buttons, radio buttons, check boxes
+#email, password, re-type password
+#first name    last name
+#male   female
+#I agree w terms and conditinos
+#i want to receive the newsletter 
+#register button
+
+#user clicks on register button, program 
+#validates user input to make sure 
+#gender is selected, both checkboxes are checked
+#password and re-type password input match up
+#password should be maksked(***)
+
+import tkinter as t 
+import tkinter.messagebox
+
+class registrationbox:
+    def __init__(self):
+        self.main_window = t.Tk()
+
+        self.main_window.geometry('300x300')
+        self.main_window.title('Responsive Registration Form')
+        
+        self.top_frame = t.Frame(self.main_window)
+        self.registration_label = t.Label(self.top_frame, text='Responsive Registration Form')
+        
+        self.email_frame = t.Frame(self.main_window)
+        self.email_label = t.Label(self.email_frame, text='Email')
+        self.email_entry = t.Entry(self.email_frame,width=40)
+    
+        self.password_frame = t.Frame(self.main_window)
+        self.password_label = t.Label(self.password_frame, text='Password')
+        self.password_entry = t.Entry(self.password_frame,width=37,show='*')
+    
+        self.password2_frame = t.Frame(self.main_window)
+        self.password2_label = t.Label(self.password2_frame, text='Re-type Password')
+        self.password2_entry = t.Entry(self.password2_frame,width=30,show='*')
+
+        self.name_frame = t.Frame(self.main_window)
+        self.fname_label = t.Label(self.name_frame, text='First Name')
+        self.fname_entry = t.Entry(self.name_frame,width=12)
+        self.lname_label = t.Label(self.name_frame, text='Last Name')
+        self.lname_entry = t.Entry(self.name_frame,width=13)
+
+        self.radio_var = t.IntVar()
+        self.gender_frame=t.Frame()
+        self.rb1=t.Radiobutton(self.gender_frame,text='Male',variable=self.radio_var,value=5)
+        self.rb1.pack(side='left')
+        self.rb2=t.Radiobutton(self.gender_frame,text='Female',variable=self.radio_var,value=10)
+        self.rb2.pack(side='right')
+
+        self.cb_terms=t.IntVar()
+        self.terms_frame = t.Frame(self.main_window)
+        self.cb1=t.Checkbutton(self.terms_frame,text='I agree with terms and conditions',variable=self.cb_terms)
+
+        self.cb_newsletter=t.IntVar()
+        self.newsletter_frame = t.Frame(self.main_window)
+        self.cb2=t.Checkbutton(self.newsletter_frame,text='I want to receive the newsletter',variable=self.cb_newsletter)
+
+        self.register_button_frame = t.Frame(self.main_window)
+        self.registerbutton = t.Button(self.register_button_frame,text='Register',command=self.validate())
+        
+        #pack
+        self.top_frame.pack()
+        self.registration_label.pack()
+
+        #pack frames onto label
+        self.email_frame.pack()
+        self.email_label.pack(side='left')
+        self.email_entry.pack(side='right')
+
+        self.password_frame.pack()
+        self.password_label.pack(side='left')
+        self.password_entry.pack(side='right')
+
+        self.password2_frame.pack()
+        self.password2_label.pack(side='left')
+        self.password2_entry.pack(side='right')
+
+        self.name_frame.pack()
+        self.fname_label.pack(side='left')
+        self.fname_entry.pack(side='left')
+        self.lname_label.pack(side='left')
+        self.lname_entry.pack()
+
+        self.gender_frame.pack()
+
+        self.cb1.pack()
+        self.terms_frame.pack()
+
+        self.cb2.pack()
+        self.newsletter_frame.pack()
+
+        self.register_button_frame.pack()
+        self.registerbutton.pack()
+
+
+        t.mainloop()
+
+    def validate(self):
+
+        if self.password_entry.get() != self.password2_entry.get():
+            tkinter.messagebox.showinfo('Passwords must match')
+        elif self.radio_var.get()== 0:
+            tkinter.messagebox.showinfo('Missing Info','Please select a gender')
+        elif self.cb_terms.get() and self.cb_newsletter.get() == 0:
+            tkinter.messagebox.showinfo('Missing Info','Please check both checkboxes')
+
+myinstance = registrationbox()
